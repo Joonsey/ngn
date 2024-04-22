7d191651627c5495da1167822357a304425a9bde
Johannes <johannes.ronning@tietoevry.com>
2024-04-22 18:43:28 +0200

    adding build option for windows

--- build-static.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p build
-
-gcc src/static.c -o build/ngn -lraylib -lm -g src/engine.c
---
+++ build.bat
@@ -0,0 +1,4 @@
+@echo off
+if not exist build mkdir build
+gcc src\static.c -o build\ngn -lraylib -lm -lgdi32 -lwinmm -g src\engine.c
+
