39fcc5459c20aa5aaf89660fac869850901a56ae
Johannes <johannes.ronning@tietoevry.com>
2024-04-24 16:38:25 +0200

    adding dynamically loaded rooms from files

--- src/room.h
+++ src/room.h
@@ -23,4 +23,4 @@ typedef struct Room {
 Room* create_room(int id, Vector4 dimensions);
 void connect_rooms(Room* room_one, Room* room_two);
 void free_room(Room* room);
-
+Room create_room_prefab(const char* filename);
